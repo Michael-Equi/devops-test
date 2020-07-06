886e6cefa81de7e818ec106fc9c4e78772c3559b
Michael Equi <michaelrequi@gmail.com>
2020-07-05 17:45:25 -0700

    Added library and unit/integration test framework

--- node/src/test-node/src/test-node.cpp
+++ node/src/test-node/src/test-node.cpp
@@ -7,6 +7,7 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "test-node/incrementer.h"
 
 using namespace std::chrono_literals;
 
@@ -28,7 +29,8 @@ private:
   void timer_callback()
   {
     auto message = std_msgs::msg::String();
-    message.data = "Hello, world!! " + std::to_string(count_++);
+    count_ = increment(count_);
+    message.data = "Hello, world!! " + std::to_string(count_);
     RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
     publisher_->publish(message);
   }
