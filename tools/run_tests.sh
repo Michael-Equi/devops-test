773dd5d266d8daba1520e99cd95836c0c8403b7c
Michael Equi <michaelrequi@gmail.com>
2020-07-05 20:55:13 -0700

    cleanup

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -5,5 +5,4 @@ source /opt/ros/foxy/setup.bash
 cd /opt/br/node || exit
 colcon test
 colcon test-result --all --verbose
-echo "############################### REPO TOKEN ${1}"
 coveralls --exclude lib --exclude tests --gcov-options '\-lp' -t "${1}"
