66797bc0d514659f187708766a44e3dc8abe24e7
Michael Equi <michaelrequi@gmail.com>
2020-07-07 20:53:36 -0700

    pushing back up

--- tools/run_tests.sh
+++ tools/run_tests.sh
@@ -5,4 +5,4 @@ source /opt/ros/foxy/setup.bash
 cd /opt/br/node || exit
 colcon test
 colcon test-result --all --verbose
-coveralls --exclude lib --exclude tests --gcov-options '\-lp' -t "${1}"
+# coveralls --exclude lib --exclude tests --gcov-options '\-lp' -t "${1}"
